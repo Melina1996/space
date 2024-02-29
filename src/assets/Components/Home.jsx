9810feafdd39d6bd47a32218caf0c070c6d19bbf
Melina1996 <melina.schlachter@gmx.de>
2024-02-29 18:31:59 +0100

    adjusted HOME

--- src/assets/Components/Home.jsx
+++ src/assets/Components/Home.jsx
@@ -5,16 +5,28 @@ export default function Home() {
   return (
     <div className='bg-bgHome w-[100vw] h-[100vh]'>
 
+        <div className='h-[20%]'>
+
         <Navbar />
 
-        <div className='flex justify-center items-center pt-28'>
-            <div className='w-[60%] flex flex-col justify-center text-white pl-44 pr-52'>
-                <h2 className='text-[28px] font-barlow'>SO, YOU WANT TO TRAVEL TO</h2>
-                <h1 className='text-[120px] tracking-wider font-nanum'>SPACE</h1>
-                <p className='text-[18px]'>Let's face it, if you want to go to space, you might as well genuinely go to outer space and not kind of hover on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+        </div>
+
+        <div className='flex justify-end items-end h-[80%] pb-20'>
+            
+            <div className='w-[50%] flex justify-end items-center text-white'>
+
+                <div className='w-[80%] flex flex-col justify-start'>
+
+                    <h2 className='text-[35px] font-barlow'>SO, YOU WANT TO TRAVEL TO</h2>
+                    <h1 className='text-[150px] tracking-wider font-nanum'>SPACE</h1>
+                    <p className='text-[18px] pr-10'>Let's face it, if you want to go to space, you might as well genuinely go to outer space and not kind of hover on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+
+                </div>
+                
             </div>
-            <div className='w-[30%] flex justify-start items-center'>
-                <div className='bg-white w-[250px] h-[250px] rounded-full flex justify-center items-center shadow-lg'>
+            <div className='w-[50%] flex justify-center items-end h-[100%] pb-10'>
+                
+                <div className='bg-white w-[280px] h-[280px] rounded-full flex justify-center items-center shadow-lg'>
 
                     <h1 className='text-black text-xl font-nanum'>EXPLORE</h1>
 
