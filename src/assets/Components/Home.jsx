ec0920d7ec944057b457e8885deb8cabbf82d841
Melina1996 <melina.schlachter@gmx.de>
2024-03-01 12:33:36 +0100

    responsive HOME

--- src/assets/Components/Home.jsx
+++ src/assets/Components/Home.jsx
@@ -11,22 +11,22 @@ export default function Home() {
 
         </div>
 
-        <div className='flex justify-end items-end h-[80%] pb-20'>
+        <div className='flex flex-col md:flex-row md:justify-end justify-start items-end h-[80%] md:pb-20'>
             
-            <div className='w-[50%] flex justify-end items-center text-white'>
+            <div className='md:w-[50%] w-[100%] flex md:justify-end justify-center items-center text-white'>
 
-                <div className='w-[80%] flex flex-col justify-start'>
+                <div className='md:w-[80%] w-[100%] flex flex-col md:justify-start justify-center items-center md:items-start'>
 
-                    <h2 className='text-[35px] font-barlow'>SO, YOU WANT TO TRAVEL TO</h2>
-                    <h1 className='text-[150px] tracking-wider font-nanum'>SPACE</h1>
-                    <p className='text-[18px] pr-10'>Let's face it, if you want to go to space, you might as well genuinely go to outer space and not kind of hover on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+                    <h2 className='xl:text-[35px] text-[25px] md:text-[25px] font-barlow'>SO, YOU WANT TO TRAVEL TO</h2>
+                    <h1 className='xl:text-[150px] text-[100px] md:text-[80px] tracking-wider font-nanum'>SPACE</h1>
+                    <p className='xl:text-[18px] text-[15px] md:pr-10 text-center'>Let's face it, if you want to go to space, you might as well genuinely go to outer space and not kind of hover on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
 
                 </div>
                 
             </div>
-            <div className='w-[50%] flex justify-center items-end h-[100%] pb-10'>
+            <div className='md:w-[50%] w-[100%] flex justify-center items-end md:h-[100%] pb-10'>
                 
-                <div className='bg-white w-[280px] h-[280px] rounded-full flex justify-center items-center shadow-lg'>
+                <div className='bg-white xl:w-[280px] xl:h-[280px] lg:w-[200px] lg:h-[200px] w-[180px] h-[180px] rounded-full flex justify-center items-center shadow-lg'>
 
                     <h1 className='text-black text-xl font-nanum'>EXPLORE</h1>
 
